1a76b031c4b3afa424c5a834ab43d7ad6612802e
booniebears <3197571813@qq.com>
2022-12-10 23:51:40 +0800

    完成登录时checkbox的置入

--- src/main/java/com/csy/controller/LoginController.java
+++ src/main/java/com/csy/controller/LoginController.java
@@ -18,16 +18,20 @@ public class LoginController {
 
     @RequestMapping("/login")
     public String login(Model model, HttpSession session, String username,
-                        String password, Boolean isAdmin) {
+                        String password, String isAdmin) {
         //可能是因为提交的问题,所以username和password可能为null;因此这里要判断一下
         if (username != null && password != null) {
-            if (adminService.login(username, password)) {
+            System.out.println("isAdmin = " + isAdmin);
+            System.out.println("username = " + username);
+            System.out.println("password = " + password);
+            if (isAdmin != null && adminService.login(username, password)) {
                 session.setAttribute("UserLoginInfo", username);
                 return "redirect:/book/allBook";
             } else {
                 model.addAttribute("login_error", "用户名或密码错误");
             }
         }
+
         return "redirect:/";
     }
 }
